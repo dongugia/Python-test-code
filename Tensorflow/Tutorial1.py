4b2849c7d3f38654c04e7df4c6d13dfd75ef7b41
dongugia <dongugia@gmail.com>
2020-09-04 08:55:30 +0900

    update

--- Tensorflow/Tutorial1.py
+++ Tensorflow/Tutorial1.py
@@ -5,14 +5,34 @@ import matplotlib.pyplot as plt
 
 data = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_lables) = data.load_data()
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover','Dress', 'Coat', 'Scandal','Shirt','Sneaker', 'Bag','Ankle boot']
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Scandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-print(train_images[6])
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
 
-plt.imshow(train_images[6], cmap=plt.cm.binary)
-plt.show()
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction" + class_names[np.argmax(prediction[i])])
+    plt.show()
+
+#print(class_names[np.argmax(prediction[0])])
+
+#test_loss, test_acc = model.evaluate(test_images, test_lables)
+
+#print("Tetsed Acc:", test_acc)
